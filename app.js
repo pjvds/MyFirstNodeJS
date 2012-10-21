414e92c434937fef1f67a39089a9a66612b4b0de
Pieter Joost van de Sande <pj@born2code.net>
2012-10-21 05:45:50 -0400

    Turn of Express layouting

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
+  app.set('view options', {layout : false});
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
